1ad7b3cd3d06677ec0f67abbf84957ae569aa9ba
cal94404 <calvin.hom@gmail.com>
2017-06-19 13:53:17 -0700

    Update host.tf

--- Instance/FSS/host.tf
+++ Instance/FSS/host.tf
@@ -6,6 +6,17 @@ resource "aws_instance" "test" {
   vpc_security_group_ids = ["${var.ssh_http_sg_id}"]
   subnet_id="${var.terraform_subnet_id}"
 
+  resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = "${aws_ebs_volume.example.id}"
+  instance_id = "${aws_instance.web.id}"
+}
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-west-2a"
+  size              = 3
+}
+  
   tags {
     Name = "terraform-FSS-two"
     SG= "${var.ssh_http_sg_id}"
