d83316958204c855da2fd5f680277bef1514bbf9
Coder63 <pbharath63@gmail.com>
2017-07-05 12:14:47 -0700

    Adding Cloud watch scale up & scale down rules

--- Cloud-Watch/Alarm.tf
+++ Cloud-Watch/Alarm.tf
@@ -33,3 +33,20 @@ resource "aws_cloudwatch_metric_alarm" "CPU-low" {
         AutoScalingGroupName = "${aws_autoscaling_group.agents.name}"
     }
 }
+
+
+resource "aws_autoscaling_policy" "agents-scale-up" {
+    name = "agents-scale-up"
+    scaling_adjustment = 1
+    adjustment_type = "ChangeInCapacity"
+    cooldown = 300
+    autoscaling_group_name = "${aws_autoscaling_group.agents.name}"
+}
+
+resource "aws_autoscaling_policy" "agents-scale-down" {
+    name = "agents-scale-down"
+    scaling_adjustment = -1
+    adjustment_type = "ChangeInCapacity"
+    cooldown = 300
+    autoscaling_group_name = "${aws_autoscaling_group.agents.name}"
+}
