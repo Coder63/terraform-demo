8d9bd3515f41001a9ec9edd419358395d41131ae
Coder63 <pbharath63@gmail.com>
2017-07-05 11:40:52 -0700

    Adding ASB & Cloud-watch features

---
+++ Auto-Scaling/App-asg.tf
@@ -0,0 +1,48 @@
+resource "aws_launch_configuration" "App-lc" {
+    name_prefix = "App-lc-"
+    image_id = "${var.ami}"
+    instance_type = "${var.instance_type}"
+
+    lifecycle {
+        create_before_destroy = true
+    }
+
+    root_block_device {
+        volume_type = "gp2"
+        volume_size = "8"
+    }
+}
+
+resource "aws_autoscaling_group" "agents" {
+    availability_zones = ["us-west-2c"]
+    name = "Agents"
+    max_size = "3"
+    min_size = "1"
+    health_check_grace_period = 300
+    health_check_type = "EC2"
+    desired_capacity = 2
+    force_delete = true
+    launch_configuration = "${aws_launch_configuration.App-lc.name}"
+
+    tag {
+        key = "Name"
+        value = "Agent Instance"
+        propagate_at_launch = true
+    }
+}
+
+resource "aws_autoscaling_policy" "agents-scale-up" {
+    name = "agents-scale-up"
+    scaling_adjustment = 1
+    adjustment_type = "ChangeInCapacity"
+    cooldown = 300
+    autoscaling_group_name = "${aws_autoscaling_group.agents.name}"
+}
+
+resource "aws_autoscaling_policy" "agents-scale-down" {
+    name = "agents-scale-down"
+    scaling_adjustment = -1
+    adjustment_type = "ChangeInCapacity"
+    cooldown = 300
+    autoscaling_group_name = "${aws_autoscaling_group.agents.name}"
+}
---
+++ Cloud-Watch/Alarm.tf
@@ -0,0 +1,35 @@
+resource "aws_cloudwatch_metric_alarm" "CPU-high" {
+    alarm_name = "CPU-util-high-agents"
+    comparison_operator = "GreaterThanOrEqualToThreshold"
+    evaluation_periods = "2"
+    metric_name = "CPUUtilization"
+    namespace = "System/Linux"
+    period = "300"
+    statistic = "Average"
+    threshold = "80"
+    alarm_description = "This metric monitors ec2 CPU for high utilization on agent hosts"
+    alarm_actions = [
+        "${aws_autoscaling_policy.agents-scale-up.arn}"
+    ]
+    dimensions {
+        AutoScalingGroupName = "${aws_autoscaling_group.agents.name}"
+    }
+}
+
+resource "aws_cloudwatch_metric_alarm" "CPU-low" {
+    alarm_name = "CPU-util-low-agents"
+    comparison_operator = "LessThanOrEqualToThreshold"
+    evaluation_periods = "2"
+    metric_name = "CPUUtilization"
+    namespace = "System/Linux"
+    period = "300"
+    statistic = "Average"
+    threshold = "40"
+    alarm_description = "This metric monitors ec2 CPU for low utilization on agent hosts"
+    alarm_actions = [
+        "${aws_autoscaling_policy.agents-scale-down.arn}"
+    ]
+    dimensions {
+        AutoScalingGroupName = "${aws_autoscaling_group.agents.name}"
+    }
+}
